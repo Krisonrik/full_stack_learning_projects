7f3d474c38ba7ff5b1f7419a986139ce1cdbc6ee
Richard Yao <richard823@gmail.com>
2019-12-18 23:39:36 -0800

    Completed Section 21: EJS

--- Web Development/todolist-v1/app.js
+++ Web Development/todolist-v1/app.js
@@ -1,66 +1,65 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const ejs = require('ejs');
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const date = require(__dirname + "/date.js");
 
 const app = express();
-var taskLists = {default: ['Buy Food', 'Cook Food', 'Eat Food']};
-// var newItems = ['Buy Food', 'Cook Food', 'Eat Food'];
+var taskLists = { default: ["Buy Food", "Cook Food", "Eat Food"] };
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-  let today = new Date();
-  let options = {weekday: 'long', day: 'numeric', month: 'long'};
-  let dayOfWeek = today.toLocaleDateString('en-US', options);
-  res.render('list', {
+app.get("/", (req, res) => {
+  let dayOfWeek = date.getDate();
+  res.render("list", {
     kindOfDay: dayOfWeek,
-    listName: 'default',
+    listName: "default",
     newListItems: taskLists.default
   });
 });
 
-app.get('/:param', (req, res) => {
-  let today = new Date();
-  let options = {weekday: 'long', day: 'numeric', month: 'long'};
-  let dayOfWeek = today.toLocaleDateString('en-US', options);
+app.get("/:param", (req, res) => {
+  let dayOfWeek = date.getDate();
   let listName = req.params.param;
-  console.log('inside ' + listName + ' get function');
-  console.log(taskLists);
-  if (taskLists.hasOwnProperty(listName)) {
-    console.log(taskLists.listName);
-    res.render('list', {
-      kindOfDay: dayOfWeek,
-      listName: listName,
-      newListItems: taskLists.listName
-    });
+  if (listName == "about") {
+    res.render("about");
   } else {
-    console.log('new list');
-    taskLists.str(listName) = [];
-    res.render('list', {
-      kindOfDay: dayOfWeek,
-      listName: listName,
-      newListItems: taskLists.listName
-    });
+    // console.log("inside " + listName + " get function");
+    // console.log(taskLists);
+    if (taskLists.hasOwnProperty(listName)) {
+      // console.log(taskLists[listName]);
+      res.render("list", {
+        kindOfDay: dayOfWeek,
+        listName: listName,
+        newListItems: taskLists[listName]
+      });
+    } else {
+      // console.log("new list");
+      taskLists[listName] = [];
+      res.render("list", {
+        kindOfDay: dayOfWeek,
+        listName: listName,
+        newListItems: taskLists[listName]
+      });
+    }
   }
 });
 
-
-app.post('/', (req, res) => {
-  console.log(req.body.listButtonName);
+app.post("/", (req, res) => {
+  // console.log(req.body.listButtonName);
   let listName = req.body.listButtonName;
   if (taskLists.hasOwnProperty(listName)) {
-    taskLists.listName.push(req.body.newItem);
-    res.redirect('/' + listName);
+    taskLists[listName].push(req.body.newItem);
+    res.redirect("/" + listName);
   } else {
-    taskLists.listName = [req.body.newItem];
-    console.log('adding new list ' + listName);
-    console.log(taskLists.listName);
-    res.redirect('/' + listName);
+    taskLists[listName] = [req.body.newItem];
+    // console.log("adding new list " + listName);
+    // console.log(taskLists[listName]);
+    res.redirect("/" + listName);
   }
 });
 
 app.listen(process.env.PORT || 3000, (error, responds, body) => {
-  console.log('Server started');
+  console.log("Server started");
 });
