d00d4da04f9aadddd16fb6ddac6413b1f8026d3c
Richard Yao <richard823@gmail.com>
2019-12-21 18:42:53 -0800

    Completed Sectio 27: Putting Everything Togather

--- Web Development/todolist-v2/app.js
+++ Web Development/todolist-v2/app.js
@@ -2,9 +2,24 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
+const _ = require("lodash");
 
 const app = express();
-var taskLists = { default: ["Buy Food", "Cook Food", "Eat Food"] };
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+mongoose.pluralize(null);
+
+const modelHolder = {};
+
+const tasksSchema = {
+  task: { type: String, required: true, min: 1, max: 255 }
+};
+
+const DefaultTasks = mongoose.model("default", tasksSchema);
+modelHolder["default"] = DefaultTasks;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -12,52 +27,80 @@ app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   let dayOfWeek = date.getDate();
-  res.render("list", {
-    kindOfDay: dayOfWeek,
-    listName: "default",
-    newListItems: taskLists.default
+
+  // mongoose.connect("mongodb://localhost:27017/todolistDB", {
+  //   useNewUrlParser: true,
+  //   useUnifiedTopology: true
+  // });
+  DefaultTasks.find((err, rslt) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("list", {
+        kindOfDay: dayOfWeek,
+        listName: "default",
+        newListItems: rslt
+      });
+    }
+    // mongoose.disconnect();
   });
 });
 
 app.get("/:param", (req, res) => {
   let dayOfWeek = date.getDate();
-  let listName = req.params.param;
+  let listName = _.lowerCase(req.params.param);
   if (listName == "about") {
     res.render("about");
   } else {
-    // console.log("inside " + listName + " get function");
-    // console.log(taskLists);
-    if (taskLists.hasOwnProperty(listName)) {
-      // console.log(taskLists[listName]);
-      res.render("list", {
-        kindOfDay: dayOfWeek,
-        listName: listName,
-        newListItems: taskLists[listName]
-      });
+    // mongoose.connect("mongodb://localhost:27017/todolistDB", {
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true
+    // });
+    let CurModel;
+    if (modelHolder.hasOwnProperty(listName)) {
+      CurModel = modelHolder[listName];
     } else {
-      // console.log("new list");
-      taskLists[listName] = [];
-      res.render("list", {
-        kindOfDay: dayOfWeek,
-        listName: listName,
-        newListItems: taskLists[listName]
-      });
+      CurModel = mongoose.model(listName, tasksSchema);
+      modelHolder[listName] = CurModel;
     }
+    CurModel.find((err, rslt) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("list", {
+          kindOfDay: dayOfWeek,
+          listName: listName,
+          newListItems: rslt
+        });
+      }
+      // mongoose.disconnect();
+    });
   }
 });
 
 app.post("/", (req, res) => {
-  // console.log(req.body.listButtonName);
   let listName = req.body.listButtonName;
-  if (taskLists.hasOwnProperty(listName)) {
-    taskLists[listName].push(req.body.newItem);
+  console.log(listName);
+  console.log(req.body.newItem);
+
+  let CurModel = modelHolder[listName];
+  let newTask = new CurModel({
+    task: req.body.newItem
+  });
+  newTask.save();
+  // mongoose.disconnect();
+  res.redirect("/" + listName);
+  console.log("completed adding");
+});
+
+app.post("/delete", (req, res) => {
+  let listName = req.body.listName;
+  let id = req.body.checkbox;
+  console.log(listName);
+  console.log(id);
+  modelHolder[listName].deleteOne({ _id: id }, () => {
     res.redirect("/" + listName);
-  } else {
-    taskLists[listName] = [req.body.newItem];
-    // console.log("adding new list " + listName);
-    // console.log(taskLists[listName]);
-    res.redirect("/" + listName);
-  }
+  });
 });
 
 app.listen(process.env.PORT || 3000, (error, responds, body) => {
